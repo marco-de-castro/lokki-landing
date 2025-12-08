437a6575fa3909de28f306e40e6f78e6c9a3901d
Marco Roberto De Castro Meza <marco@mbcolombia.co>
2025-12-08 18:55:27 -0500

    Hueco Solucionado y texto1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,75 +3,61 @@ import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <main className="w-full bg-[#E7F0E2]">
-      
+    <main className="min-h-screen flex flex-col items-center bg-[#ddecc7]">
       {/* HEADER */}
-      <section className="w-full flex justify-center">
-        <Image
-          src="/clean-page/header-container.png"
-          alt="Lokki Header"
-          width={1920}
-          height={140}
-          className="w-full h-auto"
-          priority
-        />
-      </section>
+      <Image
+        src="/clean-page/header.png"
+        alt="Lokki hero section"
+        width={1440}
+        height={900}
+        className="w-full max-w-[1440px] h-auto block"
+        priority
+      />
 
-      {/* HERO (Qué es Lokki / hero block) */}
-      <section className="w-full flex justify-center">
-        <Image
-          src="/clean-page/queesLokki.png"
-          alt="Lokki Hero"
-          width={1920}
-          height={1100}
-          className="w-full h-auto"
-          priority
-        />
-      </section>
+      {/* INFO 1 */}
+      <Image
+        src="/clean-page/info1.1.png"
+        alt="Lokki info 1"
+        width={1440}
+        height={900}
+        className="w-full max-w-[1440px] h-auto block -mt-px"
+      />
 
       {/* INFO 2 */}
-      <section className="w-full flex justify-center">
-        <Image
-          src="/clean-page/info-2.png"
-          alt="Lokki Info 2"
-          width={1920}
-          height={1100}
-          className="w-full h-auto"
-        />
-      </section>
+      <Image
+        src="/clean-page/info2.png"
+        alt="Lokki info 2"
+        width={1440}
+        height={900}
+        className="w-full max-w-[1440px] h-auto block -mt-px"
+      />
 
       {/* INFO 3 */}
-      <section className="w-full flex justify-center">
-        <Image
-          src="/clean-page/info3.png"
-          alt="Lokki Info 3"
-          width={1920}
-          height={1100}
-          className="w-full h-auto"
-        />
-      </section>
+      <Image
+        src="/clean-page/info3.png"
+        alt="Lokki info 3"
+        width={1440}
+        height={900}
+        className="w-full max-w-[1440px] h-auto block -mt-px"
+      />
 
-      {/* TESTIMONIOS */}
-      <section className="w-full flex justify-center">
-        <Image
-          src="/clean-page/testimonios.png"
-          alt="Lokki Testimonios"
-          width={1920}
-          height={600}
-          className="w-full h-auto"
-        />
-      </section>
+      {/* TESTIMONIALS */}
+      <Image
+        src="/clean-page/testimonios.png"
+        alt="Lokki testimonials"
+        width={1440}
+        height={600}
+        className="w-full max-w-[1440px] h-auto block -mt-px"
+      />
 
       {/* FOOTER */}
-      <section className="w-full flex justify-center">
-        <Image
-          src="/clean-page/footer.png"
-          alt="Lokki Footer"
-          width={1920}
-          height={260}
-          className="w-full h-auto"
-        />
-      </section>
+      <Image
+        src="/clean-page/footer.png"
+        alt="Lokki footer"
+        width={1440}
+        height={250}
+        className="w-full max-w-[1440px] h-auto block -mt-px"
+      />
     </main>
   );
 }
